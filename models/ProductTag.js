64252c54b57ad77726d15f95cac9a80146bda92c
Chad Courtney <Chadcourtney567@gmail.com>
2021-03-18 13:32:26 -0400

    did 2 models and cat routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+
   },
   {
     sequelize,
